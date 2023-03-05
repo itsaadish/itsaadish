f565be949f8b18102809703dedf1c5988bbf7b7c
aadisharma <aadish.sharma@timesinternet.in>
2023-03-05 17:17:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import Home from "./Info";
 import Aboutme from "./components/aboutme/aboutme";
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter,HashRouter ,Routes, Route, Link } from 'react-router-dom';
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
          <div className="App">
           <Home></Home>
          <Routes>
-                 <Route path='/about' element={<Aboutme />}></Route>
+                 <Route exact path='/itsaadish/about' element={<Aboutme />}></Route>
           </Routes>
         </div>  
     
-    </Router>
+    </BrowserRouter>
     
   );
 }
